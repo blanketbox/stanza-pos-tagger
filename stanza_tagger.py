c8c5a6382d6418bb5fabd2a1a8ca9cf687b9a344
Amy Amoakuh <amy.amoakuh@gmail.com>
2025-06-25 22:12:40 +0200

    tweak comments and line breaks slightly

--- stanza_tagger.py
+++ stanza_tagger.py
@@ -13,15 +13,14 @@ print("Loading language model...")
 nlp = stanza.Pipeline("en", processors="tokenize, lemma, pos", verbose=False)
 
 
-# Input file 
+# input file 
 input_file = sys.argv[1]
 
-# If the output file has not been specified the default name will be the name of the input file with the file ending vrt
+# if the output file has not been specified, the default name will be the name of the input file with the file ending vrt
 if len(sys.argv) < 3:
     output_file = "{}.vrt".format(sys.argv[1][:4])
 else: 
     output_file = sys.argv[2]
-
 
 
 print("Annotating...")
@@ -30,8 +29,8 @@ print("Annotating...")
 with open(input_file, "r", encoding="utf-8") as f:
     content = f.read()
 
-# Pattern to split XML tags and non-tags
-# Group 1: XML tags; Group 2: non-tag text
+# pattern to split XML tags and non-tags
+# group 1: XML tags, group 2: non-tag text
 parts = re.findall(r'(<[^>]+>)|([^<]+)', content)
 
 # print(parts)
@@ -39,10 +38,10 @@ parts = re.findall(r'(<[^>]+>)|([^<]+)', content)
 with open(output_file, "w", encoding="utf-8") as out:
     for tag, text in parts:
         if tag:
-            # It's an XML tag — write it unchanged
+            # it's an XML tag, write it unchanged
             out.write(tag + "\n")
         elif text:
-            # It's text content — lemmatize, clean and write
+            # it's text content, lemmatize, clean and write
             doc = nlp(text)
 
             for sentence in doc.sentences: 
